1a305b8bada3a88618a6c03a7172b5355704da19
karthikekfrazo <karthik@ekfrazo.in>
2024-11-28 11:11:06 +0530

    updated code

--- tiff_viewer_django/settings.py
+++ tiff_viewer_django/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-6uwblmh0aqx$)pwzz&u43xnyysk2aj$xtqsmkp(!te0n=mz7$j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
