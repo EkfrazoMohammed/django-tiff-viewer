785a491b8d23d0243215b92608cf015e380aba83
EkfrazoMohammed <mohammed@ekfrazo.in>
2025-01-10 20:38:55 +0530

    updated

--- tiff_viewer_django/settings.py
+++ tiff_viewer_django/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -58,6 +58,12 @@ CORS_ALLOWED_ORIGINS = [
 CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = 'tiff_viewer_django.urls'
+
+# settings.py
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 TEMPLATES = [
     {
